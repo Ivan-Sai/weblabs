02207b4f1d39e5acfb12c96cc6c2174e4287c583
Ivan <vaniasai05@gmail.com>
2024-02-26 14:02:14 +0200

    lab3

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import {MainComponent} from "./pages/main/main.component";
+import {CartComponent} from "./pages/cart/cart.component";
+import {ProductComponent} from "./pages/product/product.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "",
+    pathMatch: "full",
+    component: MainComponent,
+  },
+  {
+    path: "cart",
+    pathMatch: "full",
+    component: CartComponent,
+  },
+  {
+    path: "product/:id",
+    pathMatch: "full",
+    component: ProductComponent,
+  }
+];
